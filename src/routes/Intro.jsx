d4ba31f020e95150d609f6ed45a11ccf14ecc504
FrederikkeEbbe <119938139+FrederikkeEbbe@users.noreply.github.com>
2024-06-06 21:12:39 +0200

    Revert "ny run build"
    This reverts commit ad83e6c1d75de653cc9d974f046ddb8a9845fb0a.

--- src/routes/Intro.jsx
+++ src/routes/Intro.jsx
@@ -1,39 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import introVideo from '../assets/video/intro-video.mp4';
-import "../styles/card.scss";
-
+import React from 'react'
+import "../styles/card.scss"
+import { Link } from 'react-router-dom'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 const Intro = () => {
   return (
     <>
-      <Navbar />
-      <div className='card-container'>
-        <div className="card">
-          <h3>3 Velkommen ombord kaptajn</h3>
-          <p>Her kan du se en kort introduktion til dit nye værktøj.</p>
-          <video controls>
-            <source src={introVideo} type="video/mp4" />
-          </video>
-          <div className='progress'>
-            <Link to="/avatar">
-              <button className='left'>&#10140;</button>
-            </Link>
-            <div className='progress__mini'>
-              <div className='progress__mini--empty'></div>
-              <div className='progress__mini--empty'></div>
-              <div className='progress__mini--full'></div>
-            </div>
-            <Link to="/dashboard">
-              <button>&#10140;</button>
-            </Link>
-          </div>
+    <Navbar/>
+    <div className='card-container'>
+    <div className="card">
+      <h3>3 Velkommen ombord kaptajn</h3>
+      <p>Her kan du se en kort introdukiton til dit nye værktøj.</p>
+        <video controls>
+          <source src="/src/assets/video/intro-video.mp4" type="video/mp4" />
+        </video>
+
+        <div className='progress'>
+        <Link to="/avatar"> <button className='left'>&#10140;</button> </Link>
+        <div className='progress__mini'>
+          <div className='progress__mini--empty'></div>
+          <div className='progress__mini--empty'></div>
+          <div className='progress__mini--full'></div>
         </div>
+        <Link to="/dashboard"> <button>&#10140;</button> </Link>
       </div>
-      <Footer />
+    </div>
+    </div>
+    <Footer/>
     </>
   )
 }
 
-export default Intro;
+export default Intro
