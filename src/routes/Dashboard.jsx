25121fae842009f2532ebe2cda2f54412e070e76
FrederikkeEbbe <119938139+FrederikkeEbbe@users.noreply.github.com>
2024-06-06 16:34:17 +0200

    dashboard visualisering

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import DashboardNav from '../components/DashboardNav'
+import '../styles/dashboard.scss'
+
+// OBS: da det ikke var muligt/tilladt at hente data fra morningscore, 
+//har vi her indsat billeder der henviser til det relle produkt, 
+//med et nyt desing, fra prototypen
 
 const Dashboard = () => {
   return (
     <>
       <DashboardNav />
-      <div>Dashboard</div>
+      <section className='dashboard'>
+        <img src="/src/assets/dashboards/your-morningscore.jpg" alt="" />
+
+        <div className='dashboard--row--mid'>
+          <img src="/src/assets/dashboards/changes.jpg" alt="" />
+          <img src="/src/assets/dashboards/health.jpg" alt="" />
+          <img src="/src/assets/dashboards/links.jpg" alt="" />
+        </div>
+        <div className='dashboard--row--end'>
+          <img id='one' src="/src/assets/dashboards/missions.jpg" alt="" />
+          <img id='two' src="/src/assets/dashboards/levels.jpg" alt="" />
+        </div>
+      </section>
+
       <Footer/>
     </>
   )
